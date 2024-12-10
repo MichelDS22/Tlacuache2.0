3e8f4751b380f222d510a1036c059176fea5ddc3
BizarreSpot <138626219+BizarreSpot@users.noreply.github.com>
2024-12-09 23:32:42 -0600

    qrqerte

--- Assets/Scripts/Level_3/Parallax.cs
+++ Assets/Scripts/Level_3/Parallax.cs
@@ -7,7 +7,8 @@ public class Parallax : MonoBehaviour
     public float speed = 0.5f;  // La velocidad de desplazamiento
     public float tilingSpeed = 1f;  // La velocidad de tiling en el eje X
     private RawImage rawImage;  // El componente RawImage que contiene la textura
-
+     public float Parallax_Vel_Alpha;
+    public Player_Level_3_Controller Player;
     void Start()
     {
         rawImage = GetComponent<RawImage>();  // Obtener el componente RawImage
@@ -15,11 +16,42 @@ public class Parallax : MonoBehaviour
 
     void Update()
     {
-        // Desplazar la textura en el eje X según el tiempo y la velocidad
-        float offsetX = Time.time * speed;  // Controla el movimiento de la textura
-        rawImage.uvRect = new Rect(offsetX * tilingSpeed, 0f, 1f, 1f); // Ajustamos uvRect para movimiento
 
-        // Si la textura está configurada para hacer tiling, ajustamos la escala del RawImage
-        rawImage.uvRect = new Rect(offsetX, 0f, 1f, 1f);
+            // Desplazar la textura en el eje X según el tiempo y la velocidad
+            float offsetX = Time.time * speed;  // Controla el movimiento de la textura
+            rawImage.uvRect = new Rect(offsetX * (tilingSpeed*Parallax_Vel_Alpha), 0f, 1f, 1f); // Ajustamos uvRect para movimiento
+
+
+if (Player.Game_Status_Switch == 3)
+        {
+            Vel_Decrease(0);
+        }
+        Vel_Increase(2.5f);
     }
+    void Vel_Increase(float Value_)
+    {
+        float Value = Value_;
+
+        if (Parallax_Vel_Alpha < Value)
+        {
+            Parallax_Vel_Alpha += .0001f;
+        }
+    }
+    void Vel_Decrease(float Value_)
+    {
+        float Value = Value_;
+
+        if (Parallax_Vel_Alpha > Value)
+        {
+            Parallax_Vel_Alpha -= .01f;
+
+            if (Parallax_Vel_Alpha < Value)
+            {
+                Parallax_Vel_Alpha = Value;
+            }
+
+        }
+    }
+
+
 }
